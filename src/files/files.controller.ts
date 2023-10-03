0adc699e3fc0695420b7f6f9724a76238d4953da
flashdc7 <jrda8303@gmail.com>
2023-10-02 19:37:22 -0600

    clase-159

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -1,16 +1,27 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UploadedFile, UseInterceptors } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UploadedFile, UseInterceptors, BadRequestException } from '@nestjs/common';
 import { FilesService } from './files.service';
 import { FileInterceptor } from '@nestjs/platform-express';
+import { fileFilter } from './helpers/fileFilter.helper';
 
 @Controller('files')
 export class FilesController {
   constructor(private readonly filesService: FilesService) {}
 
   @Post('product')
-  @UseInterceptors( FileInterceptor('archivo') )
+  @UseInterceptors( FileInterceptor('archivo', {
+    fileFilter: fileFilter
+  }) )
   uploadFile( 
     @UploadedFile() file: Express.Multer.File 
     ){
-    return file
+
+      // console.log({ fileInController: file });
+      if( !file ){
+        throw new BadRequestException(`Asegurate que el archivo proporcionado es una imagen`)
+      }
+      
+    return {
+      fileName: file.originalname
+    }
   }
 }
