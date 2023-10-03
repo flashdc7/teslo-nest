a39da4c8a5f9b299801f5ef258d2879078d4a358
flashdc7 <jrda8303@gmail.com>
2023-10-03 12:30:27 -0600

    clase-160

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -2,6 +2,7 @@ import { Controller, Get, Post, Body, Patch, Param, Delete, UploadedFile, UseInt
 import { FilesService } from './files.service';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { fileFilter } from './helpers/fileFilter.helper';
+import { diskStorage } from 'multer';
 
 @Controller('files')
 export class FilesController {
@@ -9,7 +10,11 @@ export class FilesController {
 
   @Post('product')
   @UseInterceptors( FileInterceptor('archivo', {
-    fileFilter: fileFilter
+    fileFilter: fileFilter,
+    // limits: { fileSize: 1000},
+    storage: diskStorage({
+      destination: './static/uploads'
+    })
   }) )
   uploadFile( 
     @UploadedFile() file: Express.Multer.File 
