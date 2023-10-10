aa01d8a54aaec4bd47bbcd46cc2de6248036a803
flashdc7 <jrda8303@gmail.com>
2023-10-10 13:35:25 -0600

    clase-174

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,11 +3,22 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Auth } from './entities/auth.entity';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   controllers: [AuthController],
   providers: [AuthService],
   exports: [AuthService, TypeOrmModule],
-  imports: [TypeOrmModule.forFeature([Auth])]
+  imports: [
+    TypeOrmModule.forFeature([Auth]),
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.register({ 
+      secret: '1234567',
+      signOptions:{
+        expiresIn: '2h'
+      }
+    })
+  ]
 })
 export class AuthModule {}
