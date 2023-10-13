f2febdb6baf92896bcf791b4d9adb8b975c1d21f
flashdc7 <jrda8303@gmail.com>
2023-10-12 18:04:30 -0600

    clase-188

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,5 +1,6 @@
-import { BeforeInsert, BeforeUpdate, Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+import { BeforeInsert, BeforeUpdate, Column, Entity, ManyToOne, OneToMany, PrimaryGeneratedColumn } from "typeorm";
 import { ProductImage } from "./product-image.entity";
+import { Auth } from "src/auth/entities/auth.entity";
 
 
 @Entity({ name: 'products'})
@@ -57,6 +58,13 @@ export class Product {
         { cascade: true, eager: true }
     )
     images?: ProductImage[]
+
+    @ManyToOne(
+        ()=> Auth,
+        (user)=> user.product,
+        { eager: true }
+    )
+    user: Auth
 
     @BeforeInsert()
     checkSlugInsert(){
